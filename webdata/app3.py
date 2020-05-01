dcf7f5b27571ed4514c4da129ed250482fd8c6f7
georgespearing <spearingg@gmail.com>
2020-05-01 11:48:32 -0400

    update

--- webdata/app3.py
+++ webdata/app3.py
@@ -8,41 +8,39 @@ from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
 
+def get_message_value(can_id):
+        while True:
+            json_data = json.dumps(
+                    {'time':datetime.now().strftime('%y-%m-%d %H:%M:%S'), 'value': can.get_can_message(can_id)})
+            yield f"data:{json_data}\n\n"
+            time.sleep(.25)
+
 @app.route('/')
 def index():
     return render_template('layout.html')
 
 @app.route('/rideHeight')
-def rideHeight():
-    def get_message_value(can_id):
-        while True:
-            json_data = json.dumps(
-                    {'time':datetime.now().strftime('%y-%m-%d %H:%M:%S'), 'value': can.get_can_message(can_id)})
-            yield f"data:{json_data}\n\n"
-            time.sleep(1)
-
+def rideHeightTemp():
     return render_template("rideHeight.html")
 
-@app.route('/VandC/')
-def VandC():
-    def get_message_value(can_id):
-        while True:
-            json_data = json.dumps(
-                    {'time':datetime.now().strftime('%y-%m-%d %H:%M:%S'), 'value': can.get_can_message(can_id)})
-            yield f"data:{json_data}\n\n"
-            time.sleep(1)
+@app.route('/rideHeightData')
+def rideHeightData():
+    return Response(get_message_value(0x0C0))
 
+@app.route('/throttle')
+def throttleTemp():
+    return render_template('throttle.html')
+
+@app.route('/throttleData')
+def throttleData():
     return Response(get_message_value(0x0C0), mimetype='text/event-stream')
 
-@app.route('/temp/')
-def temp():
-    def get_message_value(can_id):
-        while True:
-            json_data = json.dumps(
-                    {'time':datetime.now().strftime('%y-%m-%d %H:%M:%S'), 'value': can.get_can_message(can_id)})
-            yield f"data:{json_data}\n\n"
-            time.sleep(1)
+@app.route('/steer')
+def steerTemp():
+    return render_template('steer.html')
 
+@app.route('/steerData')
+def steerData():
     return Response(get_message_value(0x0C0), mimetype='text/event-stream')
 
 
