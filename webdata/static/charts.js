46b492931ee40a419f9fa9d202cbd9b32ebb1378
georgespearing <spearingg@gmail.com>
2020-05-01 10:47:43 -0400

    update javascript

--- webdata/static/charts.js
+++ webdata/static/charts.js
@@ -44,73 +44,7 @@ $(document).ready(function () {
         }
     };
 
-    const config = {
-        type: 'line',
-        data: {
-            labels: [],
-            datasets: [{
-                label: "another damper",
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [],
-                fill: false,
-            }],
-        },
-        options: {
-            responsive: true,
-            title: {
-                display: true,
-                text: 'Creating Real-Time Charts with Flask'
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false,
-            },
-            hover: {
-                mode: 'nearest',
-                intersect: true
-            },
-            scales: {
-                xAxes: [{
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Time'
-                    }
-                }],
-                yAxes: [{
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Value'
-                    }
-                }]
-            }
-        }
-    };
-
-    const context = document.getElementById('canvas').getContext('2d');
-	var canvas = document.getElementById('canvas');
-	canvas.width = 700;
-	canvas.height = 600;
-	canvas.style.left = "100px";
-	canvas.style.top = "100px";
-	canvas.style.position = "absolute";
-
-        const lineChart = new Chart(context, config);
-
-        const source = new EventSource("/rideHeight");
-
-        source.onmessage = function (event) {
-            const data = JSON.parse(event.data);
-            if (config.data.labels.length === 20) {
-                config.data.labels.shift();
-                config.data.datasets[0].data.shift();
-            }
-            config.data.labels.push(data.time);
-            config.data.datasets[0].data.push(data.value);
-            lineChart.update();
-        }
+    
 
         const context = document.getElementById('damper2').getContext('2d');
         var canvas = document.getElementById('damper2');
